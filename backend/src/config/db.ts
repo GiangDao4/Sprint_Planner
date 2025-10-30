c013832102c55938387b399a528afaf76b25997c
GiangDao <dxgiang04@gmail.com>
2025-10-30 16:22:03 +0700

    fix: testeslint

---
+++ backend/src/config/db.ts
@@ -0,0 +1,15 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'trello',
+    password: '123456',
+    port: 5432,
+});
+
+export const query = (text: string, params?: unknown[]) => {
+    return pool.query(text, params);
+};
+
+export default pool;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,35 @@
 import express from 'express';
-
+// import db from './config/db.js';
+//import PG from 'pg';
 const app = express();
 const port = process.env.PORT || 5137;
+// import { Pool } from 'pg';
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT),
+// });
 app.use(express.json());
-
+// pool.connect().then(() => {
+//   console.log('Connected to PostgreSQL database');
+// }).catch((err) => {
+//   console.error('Error connecting to PostgreSQL database:', err);
+// });
 app.get('/', (req, res) => {
   res.json('backend is runningl');
 });
+// app.get('/api/users', async (req, res) => {
+//   try {
+//     const result = await pool.query('select * from users');
+//     res.json(result.rows);
+//   } catch (error) {
+//     console.error('Error fetching users:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
 app.get('/api/ping', (req, res) => {
   res.json('pong from backenda');
 });
