37da0f97689e21859d40b5794000b867cf007289
GiangDao <dxgiang04@gmail.com>
2025-10-31 11:42:55 +0700

    feat: connect db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,15 +1,21 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "postgres",
-  host: "127.0.0.1",
-  database: "trello",
-  password: "123456",
+  // user: "postgres",
+  // host: "127.0.0",
+  // database: "trello",
+  // password: "123456",
+  // port: 5432,
+  user: 'postgres',
+  password: '123456',
+  host: 'localhost',
   port: 5432,
+  database: 'trello'
 });
 
 export const query = (text: string, params?: unknown[]) => {
   return pool.query(text, params);
 };
+export const db = pool;
 
-export default pool;
+
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,49 +1,62 @@
 import dotenv from "dotenv";
 dotenv.config();
-console.log("Loaded ENV:", process.env);
 
+// import { db } from "./config/db.js";
 import express from "express";
-
-// import db from './config/db.js';
-// import PG from 'pg';
-const app = express();
-const port = process.env.PORT || 5137;
 import { Pool } from "pg";
 
-console.log("Password =", process.env.POSTGRES_PASSWORD);
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+
+  user: 'postgres',
+  password: '123456',
+  host: 'db',
+  port: 5432,
+  database: 'trello'
 });
 
+const app = express();
+const port = process.env.PORT || 5137;
+// import { Pool } from "pg";
+
+// console.log("Password =", process.env.DB_PASSWORD);
+
+
+
 app.use(express.json());
-pool
-  .connect()
+pool.connect()
   .then(() => {
     console.log("Connected to PostgreSQL database");
   })
-  .catch((err) => {
-    console.error("Error connecting to PostgreSQL database:", err);
+  .catch((error: unknown) => {
+    console.error("Error connecting to PostgreSQL database:", error);
   });
 app.get("/", (req, res) => {
-  res.json("backend is runningl");
+  res.json("backend is running");
 });
 app.get("/api/users", async (req, res) => {
+
+  console.log("GET /api/users called1");
   try {
     const result = await pool.query("select * from users");
     res.json(result.rows);
+    console.log("Fetched users:", result.rows);
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 app.get("/api/ping", (req, res) => {
-  res.json("pong from backenda");
+  res.json("pong from backend");
 });
+app.get("/api/a", (req, res) => {
+  res.json("vietnam");
+});
+
 app.listen(port, () => {
   console.log(` Server is listening on http://localhost:${port}`);
 });
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  next();
+});
+
